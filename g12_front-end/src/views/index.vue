5459aae8ea46e8806291243d4319eaa889e711d3
myang81 <61698214+myang81@users.noreply.github.com>
2021-03-17 16:03:10 +0800

    1
    1

---
+++ g12_front-end/src/views/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <el-container>
+            <el-header>Header</el-header>
+            <el-main>Main</el-main>
+            <el-footer>Footer</el-footer>
+        </el-container>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index",
+        props: {
+            msg: String
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>

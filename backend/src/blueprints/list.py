c179401ed3bc3a9bb11ed8fc2cf69d09da87cd2c
LtStarZq <shenlingwudi@126.com>
2021-04-17 19:38:10 +0800

    Search

--- backend/src/blueprints/list.py
+++ backend/src/blueprints/list.py
@@ -2,6 +2,11 @@ from flask import Blueprint, render_template, session, flash, redirect,request,u
 from src.extension import db
 from src.Models.Houses import House
 from src.Utility import enumMachine
+import math
+import re
+from time import time
+import os
+import json
 
 list=Blueprint('list',__name__)
 
@@ -12,6 +17,16 @@ list=Blueprint('list',__name__)
 @list.route("/test",methods=['GET','POST'])
 def test():
     return 0
+
+def calculateLength(dict):  # calculate the length of a document
+    length = 0
+    for values in dict.values():
+        length += values
+    return length
+
+
+def get_keys(d, value):
+    return [k for k, v in d.items() if v == value]
 
 
 ####################################
@@ -169,80 +184,195 @@ def getHouse():
 
         # print(direction)
         # print(request.json)
-        print("The filter requirement is:\n")
+        print("The filter requirement is:")
         print(argdict)
+        print('searchString: ', searchString)
         houses=[]
         total=0
-        if direction_listEnum == enumMachine.Direction.values:
-            houses = House.query.filter(House.price > argdict['totalPriceRange'][0],
-                                        House.price < argdict['totalPriceRange'][1],
-                                        House.floor_area > argdict['area'][0], House.floor_area < argdict['area'][1],
-                                        House._unit_price > argdict['unitPriceRange'][0],
-                                        House._unit_price < argdict['unitPriceRange'][1],
-                                        House.District.in_(argdict["district"]),
-                                        House.House_structure.in_(argdict['houseStructrue']),
-                                        #
-                                        # House.east == direction["east"], House.west == direction["west"],
-                                        # House.east_north == direction["northeast"],
-                                        # House.east_south == direction["southeast"]
-                                        # , House.north == direction["north"], House.south == direction["south"],
-                                        # House.west_south == direction["southwest"],
-                                        # House.east_south == direction["southwest"]
-                                        #,
-                                        House.Interior_design.in_(argdict["decoration"]),
-                                        House.heating.in_(argdict["heating"])
-                                        , House.elevator.in_(argdict["elevator"])
-                                        ).all()
 
+        document = {}
+        document_id = 0
+        houses = House.query.filter().all()
+        for h in houses:
+            temp_h = h.generateDetail()
+            document[document_id] = temp_h
+            document_id += 1
+        #####Search Engine
+        stopwords = set()  # A set to store the stopwords
+        with open("stopwords.txt") as f:
+            for line in f:
+                line = line[:-1]  # Remove the /n in the back of the line
+                stopwords.add(line)
 
-            total=House.query.filter(House.price >argdict['totalPriceRange'][0],House.price<argdict['totalPriceRange'][1],
-                                    House.floor_area >argdict['area'][0],House.floor_area<argdict['area'][1],
-                                  House._unit_price>argdict['unitPriceRange'][0],House._unit_price<argdict['unitPriceRange'][1],
-                                 House.District.in_(argdict["district"]),
-                                 House.House_structure.in_(argdict['houseStructrue']),
+        if not os.path.exists("home_search.txt"):  # The following code are for indexing, will only run on the first run.
+            print("This is the first time to run. The program needs document processing. Please wait a moment")
+            N = 0  # Calculate the number of documents in the collection
+            avg_doclen = 0  # The average length of a document in the collection, will be used later
+            comp = re.compile('[^a-z^0-9^ ]')  # A compiler to remove the punctuation
+            Ni = {}  # Number of documents contains term i, key is the term, value is Ni
+            Fij = {}  # Frequency of term i in document j, key is the document number, value is a dict
+            k = 1  # k for BM25
+            b = 0.75  # b for BM25
+            t1 = time()
+            for docs in document.values():
+                docs['collected'] = ''
+                docs['imgUrl'] = ''
+                docs['houseId'] = ''
+                temp_l = []
+                for v in docs.values():
+                    temp_l.append(str(v))
+                N += 1
+                line = ''.join(temp_l)
+                # line = comp.sub(' ', line.lower())
+                line = line.lower()
+                line = line.replace("-", ",")
+                line = line.replace("_", ",")
+                line = line.replace(" ", ",")
+                line = line.replace("|", ",")
+                line_split = line.split(",")
+                print(line_split)
+                # for i in range(len(line_split)):
+                #     if i != 1:
+                #         tempL = (comp.sub(' ',line_split[i])).split(' ')
+                #         line_split.remove(line_split[i])
+                #         for e in tempL:
+                #             line_split.append(e)
+                # print (line_split)
+                newDict = {}  # Store the frequency of terms in this document, key is the term, value is the frequency
+                for elements in line_split:
+                    avg_doclen += 1  # Calculate the number of terms in the document collection
+                    if elements in newDict:  # Calculate the frequency of this term in this document
+                        newDict[elements] += 1
+                    else:
+                        newDict[elements] = 1
+                for terms in newDict:  # Calculate the number of documents contains this term
+                    if terms not in Ni:
+                        Ni[terms] = 1
+                    else:
+                        Ni[terms] += 1
+                Fij[get_keys(document, docs)[0]] = newDict
+            t2 = time()
+            for terms in Ni.keys():
+                Ni[terms] = math.log2(
+                    (N - Ni[terms] + 0.5) / (Ni[terms] + 0.5))  # Calculate the value for future calculations
+            avg_doclen = avg_doclen / N  # Calculate the average doc length
+            index = {}  # Store the BM25 value of every term in every document, key is the document name, value is the BM25_dict
+            for keys in Fij.keys():
+                BM25_dict = {}  # Store the BM25 value of each term, key is the term, value is BM25 value
+                lenDj = calculateLength(Fij[keys])
+                for elements in Fij[keys].keys():
+                    BM25 = (((Fij[keys])[elements] * (1 + k)) / (
+                            (Fij[keys])[elements] + k * ((1 - b) + (b * lenDj) / avg_doclen))) * Ni[elements]
+                    BM25_dict[elements] = BM25
+                index[keys] = BM25_dict
+            js = json.dumps(index)  # use json to store the dict in the txt file
+            with open("home_search.txt", "w") as f:
+                f.write(js)
+            t3 = time()
+            print("Document processing completed")
+        ######### indexing finish
+        file = open('home_search.txt', 'r')  # open the index file and store to a dictionary
+        print("Please wait for the system to load the file")
+        t4 = time()
+        js = file.read()
+        index = json.loads(js)
+        t5 = time()
+        print("done")
+        print(t5 - t4)
+        query = '马家堡'
+        print("Enter query: ", query)
+        query = query.lower()
+        similarity = {}  # A dict store the similarity, the  key is the document id, the value is the score
+        query_term = []  # A list store the stemmed terms in the query
+        print("Result for query: ")
+        for elements in query.split(" "):
+            if elements not in stopwords:  # remove stopwords
+                query_term.append(elements)
+        for documents in index:  # calculate similarity score for every document
+            score = 0
+            for terms in query_term:
+                if terms in index[documents]:
+                    score += (index[documents])[terms]
+                similarity[documents] = score
+        result = sorted(similarity.items(), key=lambda x: x[1], reverse=True)  # Sort by similarity
+        rank = 1
+        for r in result:  # Print top 15 results
+            # print("rank: ", rank, "document: ", r[0], "score: ", r[1])
+            houseList.append(document[int(r[0])])
+            rank += 1
+            if rank == 16:
+                break
 
-                                 # House.east==direction["east"],House.west==direction["west"],House.east_north==direction["northeast"],House.east_south==direction["southeast"]
-                                 # ,House.north==direction["north"],House.south==direction["south"],House.west_south==direction["southwest"],House.east_south==direction["southwest"]
-                                 # ,
-                                House.Interior_design.in_(argdict["decoration"]),
-                                House.heating.in_(argdict["heating"])
-                                  ,House.elevator.in_(argdict["elevator"])
-                                 ).count()
-        #从数据库查找数据 在价格区间内的数据
-        else:
-            houses=House.query.filter(House.price >argdict['totalPriceRange'][0],House.price<argdict['totalPriceRange'][1],
-                                        House.floor_area >argdict['area'][0],House.floor_area<argdict['area'][1],
-                                      House._unit_price>argdict['unitPriceRange'][0],House._unit_price<argdict['unitPriceRange'][1],
-                                     House.District.in_(argdict["district"]),
-                                     House.House_structure.in_(argdict['houseStructrue']),
-                                     #
-                                     House.east==direction["east"],House.west==direction["west"],House.east_north==direction["northeast"],House.east_south==direction["southeast"]
-                                     ,House.north==direction["north"],House.south==direction["south"],House.west_south==direction["southwest"],House.east_south==direction["southwest"]
-                                     ,
-                                    House.Interior_design.in_(argdict["decoration"]),
-                                    House.heating.in_(argdict["heating"])
-                                      ,House.elevator.in_(argdict["elevator"])
-                                     ).all()
-
-            total=House.query.filter(House.price >argdict['totalPriceRange'][0],House.price<argdict['totalPriceRange'][1],
-                                    House.floor_area >argdict['area'][0],House.floor_area<argdict['area'][1],
-                                  House._unit_price>argdict['unitPriceRange'][0],House._unit_price<argdict['unitPriceRange'][1],
-                                 House.District.in_(argdict["district"]),
-                                 House.House_structure.in_(argdict['houseStructrue']),
-
-                                 House.east==direction["east"],House.west==direction["west"],House.east_north==direction["northeast"],House.east_south==direction["southeast"]
-                                 ,House.north==direction["north"],House.south==direction["south"],House.west_south==direction["southwest"],House.east_south==direction["southwest"]
-                                 ,
-                                House.Interior_design.in_(argdict["decoration"]),
-                                House.heating.in_(argdict["heating"])
-                                  ,House.elevator.in_(argdict["elevator"])
-                                 ).count()
-
-        for item in houses:
-            houseList.append(item.generateDetail())
-
-        print(houses)
-        print(total)
+        # if direction_listEnum == enumMachine.Direction.values:
+        #     houses = House.query.filter(House.price > argdict['totalPriceRange'][0],
+        #                                 House.price < argdict['totalPriceRange'][1],
+        #                                 House.floor_area > argdict['area'][0], House.floor_area < argdict['area'][1],
+        #                                 House._unit_price > argdict['unitPriceRange'][0],
+        #                                 House._unit_price < argdict['unitPriceRange'][1],
+        #                                 House.District.in_(argdict["district"]),
+        #                                 House.House_structure.in_(argdict['houseStructrue']),
+        #                                 #
+        #                                 # House.east == direction["east"], House.west == direction["west"],
+        #                                 # House.east_north == direction["northeast"],
+        #                                 # House.east_south == direction["southeast"]
+        #                                 # , House.north == direction["north"], House.south == direction["south"],
+        #                                 # House.west_south == direction["southwest"],
+        #                                 # House.east_south == direction["southwest"]
+        #                                 #,
+        #                                 House.Interior_design.in_(argdict["decoration"]),
+        #                                 House.heating.in_(argdict["heating"])
+        #                                 , House.elevator.in_(argdict["elevator"])
+        #                                 ).all()
+        #
+        #
+        #     total=House.query.filter(House.price >argdict['totalPriceRange'][0],House.price<argdict['totalPriceRange'][1],
+        #                             House.floor_area >argdict['area'][0],House.floor_area<argdict['area'][1],
+        #                           House._unit_price>argdict['unitPriceRange'][0],House._unit_price<argdict['unitPriceRange'][1],
+        #                          House.District.in_(argdict["district"]),
+        #                          House.House_structure.in_(argdict['houseStructrue']),
+        #
+        #                          # House.east==direction["east"],House.west==direction["west"],House.east_north==direction["northeast"],House.east_south==direction["southeast"]
+        #                          # ,House.north==direction["north"],House.south==direction["south"],House.west_south==direction["southwest"],House.east_south==direction["southwest"]
+        #                          # ,
+        #                         House.Interior_design.in_(argdict["decoration"]),
+        #                         House.heating.in_(argdict["heating"])
+        #                           ,House.elevator.in_(argdict["elevator"])
+        #                          ).count()
+        # #从数据库查找数据 在价格区间内的数据
+        # else:
+        #     houses=House.query.filter(House.price >argdict['totalPriceRange'][0],House.price<argdict['totalPriceRange'][1],
+        #                                 House.floor_area >argdict['area'][0],House.floor_area<argdict['area'][1],
+        #                               House._unit_price>argdict['unitPriceRange'][0],House._unit_price<argdict['unitPriceRange'][1],
+        #                              House.District.in_(argdict["district"]),
+        #                              House.House_structure.in_(argdict['houseStructrue']),
+        #                              #
+        #                              House.east==direction["east"],House.west==direction["west"],House.east_north==direction["northeast"],House.east_south==direction["southeast"]
+        #                              ,House.north==direction["north"],House.south==direction["south"],House.west_south==direction["southwest"],House.east_south==direction["southwest"]
+        #                              ,
+        #                             House.Interior_design.in_(argdict["decoration"]),
+        #                             House.heating.in_(argdict["heating"])
+        #                               ,House.elevator.in_(argdict["elevator"])
+        #                              ).all()
+        #
+        #     total=House.query.filter(House.price >argdict['totalPriceRange'][0],House.price<argdict['totalPriceRange'][1],
+        #                             House.floor_area >argdict['area'][0],House.floor_area<argdict['area'][1],
+        #                           House._unit_price>argdict['unitPriceRange'][0],House._unit_price<argdict['unitPriceRange'][1],
+        #                          House.District.in_(argdict["district"]),
+        #                          House.House_structure.in_(argdict['houseStructrue']),
+        #
+        #                          House.east==direction["east"],House.west==direction["west"],House.east_north==direction["northeast"],House.east_south==direction["southeast"]
+        #                          ,House.north==direction["north"],House.south==direction["south"],House.west_south==direction["southwest"],House.east_south==direction["southwest"]
+        #                          ,
+        #                         House.Interior_design.in_(argdict["decoration"]),
+        #                         House.heating.in_(argdict["heating"])
+        #                           ,House.elevator.in_(argdict["elevator"])
+        #                          ).count()
+        #
+        # for item in houses:
+        #     houseList.append(item.generateDetail())
+        #
+        # print(houses)
+        # print(total)
         return {
                 "success": 1,
                 "data": {
